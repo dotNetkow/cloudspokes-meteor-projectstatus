861b30c99b3a2848c34b9e7ea68489f3e4dd29da
Matt Netkow <matt.netkow@gmail.com>
2013-01-19 11:45:59 -0600

    Added ability to edit statuses as well as Titles of projects. fixed project sorting by project title -> ascending.

--- client/cloudspokes-meteor-projectstatus.js
+++ client/cloudspokes-meteor-projectstatus.js
@@ -1,46 +1,101 @@
 // Client side javascript
 Projects = new Meteor.Collection("projects");
 
-// When editing project title, ID of the project
-Session.set('editing_itemname', null);
+Session.set('editing_title', null);
+Session.set('editing_status', null);
 
 // Subscribe to 'projects' collection on startup.
 Meteor.subscribe('projects');
 
-Template.projectList.ProjectArray = function () {
-    return Projects.find({}, { sort: { Name: 1 } });
+////////// Helpers for in-place editing //////////
+
+// Returns an event map that handles the "escape" and "return" keys and
+// "blur" events on a text input (given by selector) and interprets them
+// as "ok" or "cancel".
+var okCancelEvents = function (selector, callbacks) {
+    var ok = callbacks.ok || function () { };
+    var cancel = callbacks.cancel || function () { };
+
+    var events = {};
+    events['keyup ' + selector + ', keydown ' + selector + ', focusout ' + selector] =
+      function (evt) {
+          if (evt.type === "keydown" && evt.which === 27) {
+              // escape = cancel
+              cancel.call(this, evt);
+
+          } else if (evt.type === "keyup" && evt.which === 13 ||
+                     evt.type === "focusout") {
+              // blur/return/enter = ok/submit if non-empty
+              var value = String(evt.target.value || "");
+              if (value)
+                  ok.call(this, value, evt);
+              else
+                  cancel.call(this, evt);
+          }
+      };
+    return events;
 };
 
-Template.projectList.editing = function () {
-    return Session.equals('editing_itemname', this._id);
+var activateInput = function (input) {
+    input.focus();
+    input.select();
 };
+
+/////////////////////////
+
+Template.projectList.ProjectArray = function () {
+    return Projects.find({}, { sort: { Title: 1 } });
+};
+
+Template.projectList.editingTitle = function () { return Session.equals('editing_title', this._id); };
+Template.projectList.editingStatus = function () { return Session.equals('editing_status', this._id); };
+
 
 Template.projectList.events({
-    /*'click .display' : function (event) {
-        alert('hi');
-
-    },*/
-
-    'dblclick .display': function (evt, tmpl) {
-        //alert('hey');
-        Session.set('editing_itemname', this._id);
+    'dblclick .display .pTitle': function (evt, tmpl) {
+        console.log('title');
+        Session.set('editing_title', this._id);
         Meteor.flush(); // update DOM before focus
         activateInput(tmpl.find("#pTitle"));
-    }
+    },
 
+    'dblclick .display .pStatus': function (evt, tmpl) {
+        console.log('status');
+        Session.set('editing_status', this._id);
+        Meteor.flush(); // update DOM before focus
+        activateInput(tmpl.find("#pStatus"));
+    }
 });
+
+
+Template.projectList.events(okCancelEvents(
+  '#pTitle',
+  {
+      ok: function (value) {
+          Projects.update(this._id, { $set: { Title: value } });
+          Session.set('editing_title', null);
+      },
+      cancel: function () {
+          Session.set('editing_title', null);
+      }
+  })
+);
+
+Template.projectList.events(okCancelEvents(
+  '#pStatus',
+  {
+      ok: function (value) {
+          Projects.update(this._id, { $set: { Status: value } });
+          Session.set('editing_status', null);
+      },
+      cancel: function () {
+          Session.set('editing_status', null);
+      }
+  })
+);
 
 if (Meteor.isClient) {
     Meteor.startup(function () {
         // code to run on client at startup
     });
-
-  /*
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });*/
 }
