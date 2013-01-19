5ca21a141338f36a6d8f0226a9429c91ec949408
Matt Netkow <matt.netkow@gmail.com>
2013-01-19 13:40:09 -0600

    Editing all project fields complete. New Project button complete.

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ http://www.cloudspokes.com/challenges/2000
 Keeps track of multiple Project statuses.
 
 Notes:
-To insert Project via browser console: Projects.insert({Title: "3", Status: "Cool"});
+To insert Project via browser console: 
+Projects.insert({Title: "3", DateCompleteOrig: "1/1/2013", DateCompleteCur: "2/1/2013", Status: "Behind", Notes: "this is a note."});
+
+To remove all Projects via console:
+Projects.remove({});
--- client/cloudspokes-meteor-projectstatus.js
+++ client/cloudspokes-meteor-projectstatus.js
@@ -2,12 +2,15 @@
 Projects = new Meteor.Collection("projects");
 
 Session.set('editing_title', null);
+Session.set('editing_dateOrig', null);
+Session.set('editing_dateCur', null);
 Session.set('editing_status', null);
+Session.set('editing_notes', null);
 
 // Subscribe to 'projects' collection on startup.
 Meteor.subscribe('projects');
 
-////////// Helpers for in-place editing //////////
+/* Helpers for in-place editing */
 
 // Returns an event map that handles the "escape" and "return" keys and
 // "blur" events on a text input (given by selector) and interprets them
@@ -41,29 +44,49 @@ var activateInput = function (input) {
     input.select();
 };
 
-/////////////////////////
+
+/* END Helpers for in-place editing */
+
+Template.projectList.hasProjects = function () {
+    return Projects.find().count() > 0;
+};
 
 Template.projectList.ProjectArray = function () {
     return Projects.find({}, { sort: { Title: 1 } });
 };
 
 Template.projectList.editingTitle = function () { return Session.equals('editing_title', this._id); };
+Template.projectList.editingDateOrig = function () { return Session.equals('editing_dateOrig', this._id); };
+Template.projectList.editingDateCur = function () { return Session.equals('editing_dateCur', this._id); };
 Template.projectList.editingStatus = function () { return Session.equals('editing_status', this._id); };
+Template.projectList.editingNotes = function () { return Session.equals('editing_notes', this._id); };
 
 
 Template.projectList.events({
     'dblclick .display .pTitle': function (evt, tmpl) {
-        console.log('title');
         Session.set('editing_title', this._id);
         Meteor.flush(); // update DOM before focus
         activateInput(tmpl.find("#pTitle"));
     },
-
+    'dblclick .display .pDateOrig': function (evt, tmpl) {
+        Session.set('editing_dateOrig', this._id);
+        Meteor.flush(); // update DOM before focus
+        activateInput(tmpl.find("#pDateOrig"));
+    },
+    'dblclick .display .pDateCur': function (evt, tmpl) {
+        Session.set('editing_dateCur', this._id);
+        Meteor.flush(); // update DOM before focus
+        activateInput(tmpl.find("#pDateCur"));
+    },
     'dblclick .display .pStatus': function (evt, tmpl) {
-        console.log('status');
         Session.set('editing_status', this._id);
         Meteor.flush(); // update DOM before focus
         activateInput(tmpl.find("#pStatus"));
+    },
+    'dblclick .display .pNotes': function (evt, tmpl) {
+        Session.set('editing_notes', this._id);
+        Meteor.flush(); // update DOM before focus
+        activateInput(tmpl.find("#pNotes"));
     }
 });
 
@@ -75,9 +98,29 @@ Template.projectList.events(okCancelEvents(
           Projects.update(this._id, { $set: { Title: value } });
           Session.set('editing_title', null);
       },
-      cancel: function () {
-          Session.set('editing_title', null);
-      }
+      cancel: function () { Session.set('editing_title', null); }
+  })
+);
+
+Template.projectList.events(okCancelEvents(
+  '#pDateOrig',
+  {
+      ok: function (value) {
+          Projects.update(this._id, { $set: { DateCompleteOrig: value } });
+          Session.set('editing_dateOrig', null);
+      },
+      cancel: function () { Session.set('editing_dateOrig', null); }
+  })
+);
+
+Template.projectList.events(okCancelEvents(
+  '#pDateCur',
+  {
+      ok: function (value) {
+          Projects.update(this._id, { $set: { DateCompleteCur: value } });
+          Session.set('editing_dateCur', null);
+      },
+      cancel: function () { Session.set('editing_dateCur', null); }
   })
 );
 
@@ -88,14 +131,23 @@ Template.projectList.events(okCancelEvents(
           Projects.update(this._id, { $set: { Status: value } });
           Session.set('editing_status', null);
       },
-      cancel: function () {
-          Session.set('editing_status', null);
-      }
+      cancel: function () { Session.set('editing_status', null); }
   })
 );
 
-if (Meteor.isClient) {
-    Meteor.startup(function () {
-        // code to run on client at startup
+Template.projectList.events(okCancelEvents(
+  '#pNotes',
+  {
+      ok: function (value) {
+          Projects.update(this._id, { $set: { Notes: value } });
+          Session.set('editing_notes', null);
+      },
+      cancel: function () { Session.set('editing_notes', null); }
+  })
+);
+
+function createNewProject() {
+    Projects.insert({
+        Title: "A New Project", DateCompleteOrig: "1/1/2013", DateCompleteCur: "1/1/2013", Status: "On Track", Notes: "New note."
     });
-}
+};
